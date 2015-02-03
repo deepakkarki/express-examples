988febec6fd0c8ee69d5c545d4b4c629b5ba9a4a
deepakkarki <deepak6k@gmail.com>
2015-02-03 06:55:59 +0000

    blog made dynamic

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 //this file will run the server; no other major functionality
 var express = require('express');
 var app = express();
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+var blogEngine = require('./blog');
 
-app.get("/", function(req, res) {
-			console.log("request for " + req.url);
-			res.sendFile('./views/index.html');
-		});
-
-app.get("/about", function(req, res) {
-			res.sendFile('./views/about.html');
-		});
-
-app.get("/article", function(req, res) {
-			res.sendFile('./views/article.html');
-		});
+app.use(express.static('public')); 
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(bodyParser());
+ 
+app.get('/', function(req, res) {
+    res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+});
+ 
+app.get('/about', function(req, res) {
+    res.render('about', {title:"About Me"});
+});
+ 
+app.get('/article/:id', function(req, res) {
+    var entry = blogEngine.getBlogEntry(req.params.id);
+    res.render('article',{title:entry.title, blog:entry});
+});
 
 app.get("/test", function(req, res) {
 			res.send('<h1>TEST</h1>');
